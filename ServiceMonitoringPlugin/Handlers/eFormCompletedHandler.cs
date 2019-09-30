96cb988726188b5c06a95f81f5c8cc47ce2353ca
Leonid <leonid.atf@gmail.com>
2019-09-30 21:11:28 +0300

    updates

--- ServiceMonitoringPlugin/Handlers/eFormCompletedHandler.cs
+++ ServiceMonitoringPlugin/Handlers/eFormCompletedHandler.cs
@@ -24,9 +24,18 @@ SOFTWARE.
 
 namespace ServiceMonitoringPlugin.Handlers
 {
+    using System;
+    using System.Linq;
     using System.Threading.Tasks;
+    using Helpers;
     using Messages;
+    using Microsoft.EntityFrameworkCore;
+    using Microting.eForm.Infrastructure.Constants;
+    using Microting.eForm.Infrastructure.Models;
     using Microting.EformMonitoringBase.Infrastructure.Data;
+    using Microting.EformMonitoringBase.Infrastructure.Models.Blocks;
+    using Microting.EformMonitoringBase.Infrastructure.Models.Settings;
+    using Newtonsoft.Json;
     using Rebus.Handlers;
 
     public class EFormCompletedHandler : IHandleMessages<EformCompleted>
@@ -42,6 +51,115 @@ namespace ServiceMonitoringPlugin.Handlers
         
         public async Task Handle(EformCompleted message)
         {
+            try
+            {
+                // Get settings
+                var settings = await _dbContext.PluginConfigurationValues
+                    .ToListAsync();
+                var sendGridKey = settings.FirstOrDefault(x => 
+                    x.Name == nameof(MonitoringBaseSettings.SendGridApiKey));
+                if (sendGridKey == null)
+                {
+                    throw new Exception($"{nameof(MonitoringBaseSettings.SendGridApiKey)} not found in settings");
+                }
+                var fromEmailAddress = settings.FirstOrDefault(x => 
+                    x.Name == nameof(MonitoringBaseSettings.FromEmailAddress));
+                if (fromEmailAddress == null)
+                {
+                    throw new Exception($"{nameof(MonitoringBaseSettings.FromEmailAddress)} not found in settings");
+                }
+                var fromEmailName = settings.FirstOrDefault(x => 
+                    x.Name == nameof(MonitoringBaseSettings.FromEmailName));
+                if (fromEmailName == null)
+                {
+                    throw new Exception($"{nameof(MonitoringBaseSettings.FromEmailName)} not found in settings");
+                }
+
+                var emailService = new EmailService(
+                    sendGridKey.Value,
+                    fromEmailName.Value,
+                    fromEmailAddress.Value);
+                // Get rules
+                var templateId = 1;
+                var rules = await _dbContext.Rules
+                    .AsNoTracking()
+                    .Include(x => x.Recipients)
+                    .Where(x => x.WorkflowState != Constants.WorkflowStates.Removed)
+                    .Where(x => x.TemplateId == templateId)
+                    .ToListAsync();
+
+                var caseDto = _sdkCore.CaseLookupCaseId(message.CaseId);
+                var replyElement = _sdkCore.CaseRead(caseDto.MicrotingUId, caseDto.CheckUId);
+                var dataItems = replyElement.DataItemGetAll();
+
+                // Find trigger
+                foreach (var rule in rules)
+                {
+                    var dataItemId = rule.DataItemId;
+                    var dataItem = dataItems.FirstOrDefault(x => x.Id == dataItemId);
+                    if (dataItem != null)
+                    {
+                        // Check
+                        var sendEmail = false;
+                        switch (dataItem)
+                        {
+                            case CheckBox checkBox:
+                                var block = JsonConvert.DeserializeObject<CheckBoxBlock>(rule.Data);
+                                if (checkBox.Selected == block.Selected)
+                                {
+                                    sendEmail = true;
+                                }
+                                break;
+                            case EntitySearch entitySearch:
+                                break;
+                            case EntitySelect entitySelect:
+                                break;
+                            case MultiSelect multiSelect:
+                                break;
+                            case Number number:
+                                break;
+                            case SingleSelect singleSelect:
+                                break;
+                        }
+
+                        // Send email
+                        if (sendEmail)
+                        {
+                            if (rule.AttachReport)
+                            {
+                                foreach (var recipient in rule.Recipients)
+                                {
+                                    // TODO Get report file
+                                    string fileName = "";
+                                    await emailService.SendFileAsync(
+                                        rule.Subject,
+                                        recipient.Email,
+                                        rule.Text,
+                                        fileName);
+                                }
+                            }
+                            else
+                            {
+                                foreach (var recipient in rule.Recipients)
+                                {
+                                    await emailService.SendAsync(
+                                        rule.Subject,
+                                        recipient.Email,
+                                        rule.Text);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
---
+++ ServiceMonitoringPlugin/Helpers/EmailService.cs
@@ -0,0 +1,76 @@
+﻿namespace ServiceMonitoringPlugin.Helpers
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using SendGrid;
+    using SendGrid.Helpers.Mail;
+
+    public class EmailService
+    {
+        private readonly string _sendGridApiKey;
+        private readonly string _fromEmailAddress;
+        private readonly string _fromEmailName;
+
+        public EmailService(
+            string sendGridApiKey,
+            string fromEmailAddress,
+            string fromEmailName)
+        {
+            _sendGridApiKey = sendGridApiKey;
+            _fromEmailAddress = fromEmailAddress;
+            _fromEmailName = fromEmailName;
+        }
+
+        public async Task SendAsync(
+            string subject,
+            string to,
+            string text = null,
+            string html = null)
+        {
+            try
+            {
+                var client = new SendGridClient(_sendGridApiKey);
+                var fromAddress = new EmailAddress(_fromEmailAddress, _fromEmailName);
+                var toAddress = new EmailAddress(to);
+                var msg = MailHelper.CreateSingleEmail(fromAddress, toAddress, subject, text, html);
+                var response = await client.SendEmailAsync(msg);
+                if (((int)response.StatusCode < 200) || ((int)response.StatusCode >= 300))
+                {
+                    throw new Exception($"Status: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to send email message", ex);
+            }
+        }
+
+        public async Task SendFileAsync(string subject, string to, string text, string fileName, string html = "")
+        {
+            try
+            {
+                var client = new SendGridClient(_sendGridApiKey);
+                var fromEmail = new EmailAddress(_fromEmailAddress, _fromEmailName);
+                var toEmail = new EmailAddress(to);
+                var msg = MailHelper.CreateSingleEmail(fromEmail, toEmail, subject, text, html);
+                var bytes = File.ReadAllBytes(fileName);
+                var file = Convert.ToBase64String(bytes);
+                msg.AddAttachment(Path.GetFileName(fileName), file);
+                var response = await client.SendEmailAsync(msg);
+                if (((int)response.StatusCode < 200) || ((int)response.StatusCode >= 300))
+                {
+                    throw new Exception($"Status: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to send email message", ex);
+            }
+            finally
+            {
+                File.Delete(fileName);
+            }
+        }
+    }
+}
